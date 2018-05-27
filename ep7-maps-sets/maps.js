387c893737b6fcc5d7e3ea8e46a78a0cda174a4c
Medhat Dawoud <medhatdawoud@gmail.com>
2018-05-27 14:16:02 +0200

    ep7 maps and sets

---
+++ ep7-maps-sets/maps.js
@@ -0,0 +1,56 @@
+let hashObj = new Object();
+
+hashObj['key'] = 'val';
+
+console.log(hashObj['key'] !== undefined);
+
+
+// Example of Months Map
+
+// let months = new Map();
+
+// months.set(1, 'Jan');
+// months.set(2, 'Feb');
+// months.set(3, 'Mar');
+// months.set(4, 'Apr');
+// months.set(5, 'May');
+// months.set(6, 'Jun');
+// months.set(7, 'Jul');
+// months.set(8, 'Aug');
+// months.set(9, 'Sep');
+// months.set(10, 'Oct');
+// months.set(11, 'Nov');
+// months.set(12, 'Dec');
+
+// let whatIsTheCurrentMonth = () => {
+//   let date = new Date();
+//   return months.get(date.getMonth() + 1);
+// };
+
+// console.log(whatIsTheCurrentMonth());
+
+
+
+
+
+
+
+
+
+
+
+
+// let obj = [
+//   [1, 'Jan'],
+//   [2, 'Feb'],
+//   [3, 'Mar'],
+//   [4, 'Apr'],
+//   [5, 'May'],
+//   [6, 'Jun'],
+//   [7, 'Jul'],
+//   [8, 'Aug'],
+//   [9, 'Sep'],
+//   [10, 'Oct'],
+//   [11, 'Nov'],
+//   [12, 'Dec']
+// ];
---
+++ ep7-maps-sets/sets.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5, 6, 6];
+
+let set = new Set(arr);
+
+// set.add('val1');
+// set.add('val2');
+
+console.log(set);
