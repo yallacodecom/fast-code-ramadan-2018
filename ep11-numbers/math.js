2b1404e770ed7ee6473fdbf9a2ea6b69c7520bb9
Medhat Dawoud <medhatdawoud@gmail.com>
2018-06-07 19:47:48 +0200

    ep11 - Numbers

---
+++ ep11-numbers/math.js
@@ -0,0 +1,6 @@
+let num = 27;
+
+console.log(3**3);
+// console.log(Math.sign(0))
+
+
---
+++ ep11-numbers/numbers.js
@@ -0,0 +1,27 @@
+// let number = '0x11';
+
+// // console.log(Number(number));
+
+// function toRGB(colorCode) {
+//   let r = '0x'+colorCode.substr(0, 2);
+//   let g = '0x'+colorCode.substr(2, 2);
+//   let b = '0x'+colorCode.substr(4, 2);
+  
+//   return `rgb(${Number(r)},${Number(g)},${Number(b)})`;
+// }
+
+// function toHex(r, g, b) {
+//   return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+// }
+
+// console.log(toHex(254,250,103));
+
+
+// Number.EPSILON
+// console.log(0.2+0.1);
+
+// function epsEqu(x, y) {
+//   return Math.abs(x - y) < Number.EPSILON;
+// }
+// console.log(epsEqu(0.3 , (0.1 + 0.2)));
+// console.log(Number.EPSILON);
